68df3e5a837e2e48d8314d9f5ebb032d2aa626cf
mitalletap <54956315+mitalletap@users.noreply.github.com>
2020-04-07 01:54:46 -0500

    Adding Pathfinding! Dijkstra can detect walls (except for the borders), and can find the shortest path from the start to the end node.

--- src/Pathfinding/Mazes/Patterns.js
+++ src/Pathfinding/Mazes/Patterns.js
@@ -1,22 +1,46 @@
 export function smilyFaceMaze(grid){
     var visited = []
-    var values = [7, 8, 16, 17, 18];
+    var current = grid[10][10];
+    current.wall = true;
+    visited.push(current)
+    // for(var x = 16; x <=19; x++){
 
-    for(var x = 0; x < 19; x++){
-        if(values.includes(x)){
-            visited.push(grid[28][x])
-            visited.push(grid[29][x])
-            visited.push(grid[39][x])
-            visited.push(grid[40][x])
-        }
-    }
+    //     grid[x][4].wall = true;
+    //     grid[x][5].wall = true;
+    //     grid[x][6].wall = true;
+    //     grid[x][7].wall = true;
+    //     grid[x][18].wall = true;
+    //     grid[x][19].wall = true;
+    //     grid[x][20].wall = true;
+    //     grid[x][21].wall = true;
 
-    for(var x = 30; x <= 38; x++){
-        visited.push(grid[x][18])
-    }
+    //     visited.push(grid[x][4])
+    //     visited.push(grid[x][5])
+    //     visited.push(grid[x][6])
+    //     visited.push(grid[x][7])
+    //     visited.push(grid[x][18])
+    //     visited.push(grid[x][19])
+    //     visited.push(grid[x][20])
+    //     visited.push(grid[x][21])
+    // }
 
-    for(var x = 30; x <= 38; x++){
-        visited.push(grid[x][19])
-    }
+
+    // for(var x = 25; x <=28; x++){
+    //     visited.push(grid[x][4])
+    //     visited.push(grid[x][5])
+    //     visited.push(grid[x][6])
+    //     visited.push(grid[x][7])
+    //     visited.push(grid[x][18])
+    //     visited.push(grid[x][19])
+    //     visited.push(grid[x][20])
+    //     visited.push(grid[x][21])
+    // }
+
+    // for(var x = 20; x <= 25; x++){
+    //     visited.push(grid[x][20])
+    //     visited.push(grid[x][21])
+    //     visited.push(grid[x][22])
+    // }
+    console.log(visited);
     return visited;
 }
--- src/Pathfinding/Pathfinding.jsx
+++ src/Pathfinding/Pathfinding.jsx
@@ -1,16 +1,17 @@
 import './Pathfinding.css'
 import React, { Component } from 'react';
 import Cell from '../Cells/Cell';
-
-import { NodeIndexOutlined, NodeExpandOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import { NodeIndexOutlined, NodeExpandOutlined, ClearOutlined, SmileOutlined } from '@ant-design/icons';
 import { Layout, Menu, Button } from 'antd';
+import { dijkstra, getShortestPath } from './Algorithms/Dijkstra'
+import { recursiveDivisionMaze } from './Mazes/RecusiveDivision'
+import { recursiveDivisionRandomizerMaze } from './Mazes/RecursiveDivisionRandomizer'
+import { smilyFaceMaze } from './Mazes/Patterns'
+import { wait } from '@testing-library/react';
 
 const { SubMenu } = Menu;
-const { Header, Sider, Content } = Layout;
-
-
-
-
+const { Header, Footer, Sider, Content } = Layout;
 class Pathfinding extends Component {
     constructor(props) {
         super(props);
@@ -29,8 +30,8 @@ class Pathfinding extends Component {
             mouseHold: false,
             holdingStart: false,
             holdingEnd: false,
-            maxCols: 25, //70
-            maxRows: 15,  //40
+            maxCols: 47, //47
+            maxRows: 27,  //27
             activeMaze: false,
             activeAlgorithm: false,
             active: false,
@@ -67,11 +68,6 @@ class Pathfinding extends Component {
         }
     }
 
-    getCell = (grid, col, row) => {
-        const cell = grid[col][row];
-        return cell;
-    }
-
     createWall = (grid, col, row) => {
         const newGrid = grid.slice();
         const cell = this.getCell(newGrid, col, row);
@@ -83,13 +79,20 @@ class Pathfinding extends Component {
         return newGrid;
     }
 
+    getCell = (grid, col, row) => {
+        const cell = grid[col][row];
+        return cell;
+    }
+
     setWall = (grid, walls) => {
+        console.log(walls)
         var x = 0; 
         while(x < walls.length){
             for(var col = 0; col < this.state.maxCols; col++){
                 for(var row = 0; row < this.state.maxRows; row++){
-                    if(walls[x].row === grid[col][row] && walls[x].col === grid[col][row] && walls[x].wall === true){
+                    if(walls[x].row === grid[col][row] && walls[x].col === grid[col][row] ){
                         grid[col][row].wall = true;
+                        console.log(`Wall at ${grid[col][row]}`)
                     }
                 }
             }
@@ -182,7 +185,6 @@ class Pathfinding extends Component {
         this.setState({ mouseDown: false, mouseUp: true, holdingEnd: false, holdingStart: false, destroyingWall: false })
     }
     
-
     handleClearPath(grid){
         if(this.state.active === false){
             this.setState({ activeAlgorithm: false })
@@ -225,9 +227,10 @@ class Pathfinding extends Component {
     }
 
     drawBorders(col, row){
+        // 20 10
         var mazeWalls = []
 
-        // TOP BORDER
+        // TOP BORDER        
         for(var i = col - 1; i > 0; i--){
             mazeWalls.push({
                 row: i,
@@ -235,27 +238,27 @@ class Pathfinding extends Component {
                 wall: true
             })
         }
-    
-        // LEFT BORDER
-        for(var i = 0; i < row - 1; i++){
+
+        // LEFT BORDER        
+        for(var i = 0; i < row; i++){
             mazeWalls.push({
                 row: 0,
                 col: i,
                 wall: true
             })
         }
-    
-        // BOTTOM BORDER
-        for(var i = 0; i < col - 1; i++){
+
+        // BOTTOM BORDER        
+        for(var i = 0; i < col; i++){
             mazeWalls.push({
                 row: i,
                 col: row - 1,
                 wall: true
             })
         }
-    
-        // RIGHT BORDER
-        for(var i = row - 1; i > 0 - 1; i--){
+
+        // RIGHT BORDER        
+        for(var i = row - 1; i >= 0; i--){
             mazeWalls.push({
                 row: col - 1,
                 col: i,
@@ -265,63 +268,176 @@ class Pathfinding extends Component {
         return mazeWalls;
     }
 
+    // Switch case to vizualize multiple algorithms
+    visualizeAlgorithm(algorithm) {
+        const { grid, startRow, startCol, endRow, endCol, maxCols, maxRows } = this.state;
+        const start = grid[startRow][startCol];
+        const end = grid[endRow][endCol];
+        var visited;
+        switch(algorithm){
+            case 'Dijkstra':
+                visited = dijkstra(grid, start, end);
+                const shortestPath = getShortestPath(end);
+                this.animateDijkstra(visited, shortestPath);
+                break;
+            // case 'AStar':
+            //     astar(grid, start, end);
+            //     break;
+            default:
+                console.log("Algorithm Not Found");
+                break;
+        }
 
+    }    
+    
+    visualizeMaze(maze) {
+        const { grid, maxRows, maxCols } = this.state;
+        const visited = this.drawBorders(maxCols, maxRows);
+        var newVisited;
+        var newGrid = [];
+        switch(maze){
+            case 'Recursive Division':
+                newVisited = recursiveDivisionMaze(maxCols, maxRows);
+                break;
+            case "Recursive Division Random":
+                newVisited = recursiveDivisionRandomizerMaze(grid, maxRows, maxCols);
+                break;
+            case 'Smily':
+                newVisited = smilyFaceMaze(grid);
+                break;
+            default:
+                console.log("Maze Not Found");
+        }
+        // var waitTime = 
+        var waitTime = this.animateMaze(visited.concat(newVisited));
+        setTimeout(() => {
+            newGrid = this.setWall(grid, visited);
+            console.log(newGrid)
+            this.setState({ grid: newGrid })
+            console.log(this.state.grid)
+            console.log("State set")
+        }, 10 * waitTime);
+
+    }
+
+    // Animates every "CELL" that is apart of the shortest path.
+    // Also re-animates the start and end
+    animateShortestPath(shortestPath) {
+        for (let i = 0; i < shortestPath.length; i++){
+            setTimeout(() => {
+                const cell = shortestPath[i];
+                document.getElementById(`cell-${cell.col}-${cell.row}`).classList.add('cell-shortest-path');
+            }, 50 * i);
+        }
+        var endCell = shortestPath[shortestPath.length - 1];
+        if(endCell !== undefined){ 
+            this.setState({ pathFound: true });
+        }
+        this.setState({ simulationComplete: true })
+    }
+
+    // Animates all every "CELL" that is not apart of the shortest path, the start, or the end
+    animateDijkstra(visited, shortestPath) {
+        for (let i = 0; i <= visited.length; i++){
+            if(i === visited.length) {
+                setTimeout(() => {
+                    this.animateShortestPath(shortestPath);
+                    this.setState({ active: false })
+                }, 10 * i);
+                return;
+            }
+            setTimeout(() => {
+                const cell = visited[i];
+                document.getElementById(`cell-${cell.col}-${cell.row}`).classList.add('cell-visited');
+            }, 10 * i);
+        }
+        
+        
+    }
+
+    animateMaze(visited) {
+        for(var i = 0; i < visited.length - 1; i++){
+            const cell = visited[i];
+            if(cell.wall === true){
+                setTimeout(() => {
+                    document.getElementById(`cell-${cell.col}-${cell.row}`).classList.add('cell-wall');
+                }, 10 * i);
+            }
+        }
+        console.log("animated!")
+        return i;
+    }
+
+
+
+
+
+    onCollapse = collapsed => {
+        this.setState({ collapsed });
+    };
 
     render() { 
         const { grid } = this.state;
         const min = 1;
         return ( 
+
+            
             <React.Fragment>
-                <div className="grid" style={{ display: "inline"}}>
-                    {grid.map((row, rowId) => {
-                        return (
-                                <div key={rowId} className={"col"}>
-                                {row.map((cell, cellId) => {
-                                    const { row, col, startCell, endCell, wall, visited, mouseDown } = cell;
+                 <Layout style={{ minHeight: '100vh' }}>
+                    <Header> <h1 className="site-header">PATHFINDING VISUALIZER</h1></Header>
+                    <Layout>
+                        <Sider width={"15vw"} collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+                            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                                <SubMenu key="sub1" title={<React.Fragment> <NodeIndexOutlined /> <span> Pathfinding </span></React.Fragment>}>
+                                    <Menu.Item key="1" onClick={() => this.setState({ selectedAlgorithm: 'Dijkstra' })}> Dijkstra </Menu.Item>
+                                    <Menu.Item key="2" onClick={() => this.setState({ selectedAlgorithm: 'Dijkstra' })}> A* </Menu.Item>
+                                    <Menu.Item key="3"> Other</Menu.Item>
+                                </SubMenu>
+                                <SubMenu key="sub2" title={<React.Fragment> <NodeExpandOutlined /> <span> Maze </span> </React.Fragment>}>
+                                    <Menu.Item key="4" onClick={() => this.setState({ selectedMaze: 'Recursive Division' })}> Recursive Division </Menu.Item>
+                                    <Menu.Item key="5" onClick={() => this.setState({ selectedMaze: 'Recursive Backtracking' })}> Recursive Backtracking </Menu.Item>
+                                    <SubMenu key="sub3" title={<React.Fragment> <SmileOutlined /> <span> Randomize </span> </React.Fragment>}>
+                                        <Menu.Item key="6" onClick={() => this.setState({ selectedMaze: 'Smily' })}> Smily Face </Menu.Item>
+                                        <Menu.Item key="7" onClick={() => this.setState({ selectedMaze: 'Recursive Backtracking' })}> Recursive Backtracking </Menu.Item>
+                                    </SubMenu>
+                                </SubMenu>
+                                <Menu.Item key="8" disabled={this.state.selectedAlgorithm === "" || this.state.activeAlgorithm === true} onClick={() => { this.visualizeAlgorithm(this.state.selectedAlgorithm); this.setState({ activeAlgorithm: true })}}><ClearOutlined /> <span> {this.state.selectedAlgorithm === "" ? "Choose an Algorithm" : `Simulate ${this.state.selectedAlgorithm}` } </span></Menu.Item>
+                                <Menu.Item key="9" disabled={this.state.selectedMaze === "" || this.state.activeMaze === true} onClick={() => { this.visualizeMaze(this.state.selectedMaze); this.setState({ activeMaze: true })}}><ClearOutlined /> <span> {this.state.selectedMaze === "" ? "Choose an Maze" : `Simulate ${this.state.selectedMaze}` } </span></Menu.Item>
+                                <Menu.Item key="10" onClick={() => this.handleClearBoard()}><ClearOutlined /> <span> Clear Board </span></Menu.Item>
+                            </Menu>
+                        </Sider>
+                        <Content style={{ paddingTop: "10px"}}>
+                            <div className="grid" style={{ display: "inline"}}>
+                                {grid.map((row, rowId) => {
                                     return (
-                                        <Cell 
-                                            key={cellId}
-                                            row={col}
-                                            col={row}
-                                            startCell={startCell}
-                                            endCell={endCell}
-                                            wall={wall}
-                                            visited={visited}
-                                            mouseDown={mouseDown}
-                                            onMouseDown={(col, row) => this.handleMouseDown(col, row)}
-                                            onMouseEnter={(col, row) => this.handleMouseEnter(col, row)}
-                                            onMouseUp={(col, row) => this.handleMouseUp(col, row)}
-                                        />
+                                            <div key={rowId} className={"col"}>
+                                            {row.map((cell, cellId) => {
+                                                const { row, col, startCell, endCell, wall, visited, mouseDown } = cell;
+                                                return (
+                                                    <Cell 
+                                                        key={cellId}
+                                                        row={col}
+                                                        col={row}
+                                                        startCell={startCell}
+                                                        endCell={endCell}
+                                                        wall={wall}
+                                                        visited={visited}
+                                                        mouseDown={mouseDown}
+                                                        onMouseDown={(col, row) => this.handleMouseDown(col, row)}
+                                                        onMouseEnter={(col, row) => this.handleMouseEnter(col, row)}
+                                                        onMouseUp={(col, row) => this.handleMouseUp(col, row)}
+                                                    />
+                                                );
+                                            })}
+                                        </div>
                                     );
                                 })}
                             </div>
-                            
-                        );
-                    })}
-                </div>
-                <div>
-                    {`MouseUp: ${this.state.mouseUp}`}
-                    <br/>
-                    {`MouseDown: ${this.state.mouseDown}`}
-                    <br/>
-                    {`MouseHold: ${this.state.mouseHold}`}
-                    <br/>
-                    {`HoldingStart: ${this.state.holdingStart}`}
-                    <br/>
-                    {`HoldingEnd: ${this.state.holdingEnd}`}
-                    <br/>
-                    {/* {`Start Col-Row: ${this.state.startCol} - ${this.state.startRow}`}
-                    <br/>
-                    {`Previous Start Col-Row: ${this.state.previousStartCol} - ${this.state.previousStartRow}`}
-                    <br/>
-                    {`End Col-Row: ${this.state.endCol} - ${this.state.endRow}`}
-                    <br/>
-                    {`Previous End Col-Row: ${this.state.previousEndCol} - ${this.state.previousEndRow}`} */}
-                    <br/>
-                    {`DestroyingWall: ${this.state.destroyingWall}`}
-                    <br/>
-                    <button onClick={() => this.handleClearBoard()}>Celar</button>
-                </div>
+                        </Content>
+                    </Layout>
+                </Layout>
+                
+                
 
             </React.Fragment>
          );
