cfc533417626b67cf7d5e78590dfaae90eb4672b
mitalletap <54956315+mitalletap@users.noreply.github.com>
2020-05-13 20:15:44 -0500

    Server Commands

---
+++ build/precache-manifest.25759932f97f2ad0795ce9d4f90bfde4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9be7efdcfbc434666307ba576c6b62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08bce0da7b0ce50805d7",
+    "url": "/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "6fc63ab40f705ab42f0b",
+    "url": "/static/css/main.3f3c8d0f.chunk.css"
+  },
+  {
+    "revision": "08bce0da7b0ce50805d7",
+    "url": "/static/js/2.f52dad00.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/static/js/2.f52dad00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc63ab40f705ab42f0b",
+    "url": "/static/js/main.68ab9dea.chunk.js"
+  },
+  {
+    "revision": "46dfd5efdce9fd3554c0",
+    "url": "/static/js/runtime-main.481875b0.js"
+  }
+]);
